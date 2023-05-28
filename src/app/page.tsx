6e08b17978644cc6d32f2e778eca7fcce081df70
Marcus Bernroth <marcusbernroth@gmail.com>
2023-05-28 22:04:26 +0200

    Revert "Try out ResRobot API"
    This reverts commit 89df095ce5d75d57eeec3679be1d6ae3d10b9462.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,20 @@
-import React from "react";
-import { JSDOM } from "jsdom";
-import Timetable from "./components/Timetable";
-
-interface MenuData {
-  menu: string[];
-}
-
-async function getMenu() {
-  const url = "http://carnex.se/";
-  const res = await fetch(url);
-  const text = await res.text();
-  const { document } = new JSDOM(text).window;
-
-  const html = document.querySelectorAll(".second-home-block .day");
-  let menu = "";
-
-  html.forEach((e) => (menu += e.textContent));
-  let stripped = menu
-    .replace(/\t/g, "")
-    .split("\n")
-    .filter((s) => s !== "");
-  return stripped;
-}
-
-const Menu = ({ menu }: MenuData) => {
-  return (
-    <>
-      {menu.map((s, i) =>
-        !(i % 2) ? (
-          <h3 className="font-bold">{s}</h3>
-        ) : (
-          <p className="indent-2">{s}</p>
-        )
-      )}
-    </>
-  );
-};
+import React, { Suspense } from "react";
+import useSWR from "swr";
+import menu from "../../pages/api/menu";
 
 export default async function Home() {
-  const menu = await getMenu();
-
+  const data = menu();
   return (
     <div className="grid grid-cols-2 gap-4 flex-1 grow">
-      <div>
-        <Menu menu={menu} />
-      </div>
-      <div>
-        <Timetable />
-      </div>
+      {data && <Menu menu={data} />}
     </div>
   );
+}
+
+interface MenuData {
+  menu: string;
+}
+
+function Menu({ menu }: MenuData) {
+  return <>{JSON.stringify(menu)}</>;
 }
